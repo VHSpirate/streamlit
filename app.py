52b855c612bec06955f08e042ef842adba218395
VHSpirate <comandanteashtarsheran33@gmail.com>
2021-11-05 19:25:52 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,10 @@
 import streamlit as st
 
-x= st.slider('slect a value')
+x = st.slider('Select a value')
+st.write(x, 'squared is', x * x)
+
+st.sidebar.checkbox('Click me')
+
+st.button('Click')
+
+st.markdown(':+1:')
