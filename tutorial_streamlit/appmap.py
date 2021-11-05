bfe9e5ea63c4b1c5acfb549e76f940003cfda2aa
VHSpirate <comandanteashtarsheran33@gmail.com>
2021-11-05 19:52:06 -0300

    demo1

---
+++ tutorial_streamlit/appmap.py
@@ -0,0 +1,18 @@
+import streamlit as st
+import pandas as pd
+import altair as alt
+
+
+#make a title for this demo using emojis
+st.title('DataFrame Demo')
+
+## Elements on the sidebar
+#st.sidebar.selectbox('Choose a demo', ['DataFrame Demo', 'Other'])
+#check=st.sidebar.checkbox('Show code')
+
+#write some text describing the app
+'''
+This is a demo shows how to use `st.write` to visualize Pandas DataFrames
+
+[Data courtesy of the [UN Data Explorer](http://data.un.org/Data.aspx?d=FAO&f=itemCode%3a2051).)
+'''
