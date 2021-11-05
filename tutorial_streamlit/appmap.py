5c9fd05b62bd8cc131ab60e4a76c39cc396e961d
VHSpirate <comandanteashtarsheran33@gmail.com>
2021-11-05 20:01:06 -0300

    testpaises

--- tutorial_streamlit/appmap.py
+++ tutorial_streamlit/appmap.py
@@ -16,3 +16,12 @@ This is a demo shows how to use `st.write` to visualize Pandas DataFrames
 
 [Data courtesy of the [UN Data Explorer](http://data.un.org/Data.aspx?d=FAO&f=itemCode%3a2051).)
 '''
+df = pd.read_csv('data1.csv')
+
+# make a list with the countries in the dataframe
+countries = df['Country or Area'].unique()
+#st.write(countries)
+
+##make a multiselect box with the countries
+country_select = st.multiselect('Choose Countries', countries, default=['Chile','Mexico'])
+#st.write(country_select)
